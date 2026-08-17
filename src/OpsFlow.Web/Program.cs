26cf3d4bc74bdd6dce248936387f23dd56171d58
Mateus Barbosa <37279759+barbosamg@users.noreply.github.com>
2026-08-16 21:05:28 -0300

    feat(web): integrate Telerik UI foundation

--- src/OpsFlow.Web/Program.cs
+++ src/OpsFlow.Web/Program.cs
@@ -1,10 +1,13 @@
 using OpsFlow.Web.Components;
+using Telerik.Blazor.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddTelerikBlazor();
 
 var app = builder.Build();
 
